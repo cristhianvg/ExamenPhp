de8ae36443c82545f175197333032ba7d71d96e2
cristhianvg <cristhianvg1991@gmail.com>
2017-05-30 16:41:43 -0500

    PROGRESO ExamenPhp
    1.Creacion de Permisos
    1.1 Se crearon los middleware para el control de permisos segun su
    session
    
    2.Creacion de Logout
    2.1 Quedo funcionando al 100% el logout

--- www/app/app.config.js
+++ www/app/app.config.js
@@ -1,16 +1,38 @@
 angular.module('ExamenPhp').constant('rolAdmin', 1);
 angular.module('ExamenPhp').constant('rolCelador', 2);
 
+/**
+ * middleware que comprueba las session y los tipos de roles
+ */
+angular.module('ExamenPhp').config(['$middlewareProvider', function middlewareProviderConfig($middlewareProvider) {
+        $middlewareProvider.map({
+            'comprobarSession': ['$sessionStorage', function comprobarSession($sessionStorage) {
+                    middlewareComprobarSession(this, $sessionStorage);
+                }],
+            'comprobarNoTenerSession': ['$sessionStorage', function comprobarNoTenerSession($sessionStorage) {
+                    middlewareComprobarNoTenerSession(this, $sessionStorage);
+                }]
+        });
+    }]);
+
+
 angular.module('ExamenPhp').config(['$routeProvider', '$httpProvider', function config($routeProvider, $httpProvider) {
         $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
         $routeProvider.
                 when('/', {
                     controller: 'loginController',
-                    templateUrl: 'app/template/login.html'
+                    templateUrl: 'app/template/login.html',
+                    middleware: ['comprobarNoTenerSession']
                 }).
                 when('/inicio', {
                     controller: 'inicioController',
-                    templateUrl: 'app/template/inicio.html'
+                    templateUrl: 'app/template/inicio.html',
+                    middleware: ['comprobarSession']
+                }).
+                when('/logout', {
+                    controller: 'logoutController',
+                    template: '<p>Cerrando sesión...</p>',
+                    middleware: ['comprobarSession']
                 }).
                 otherwise('/');
     }
---
+++ www/app/controller/controller.logout.js
@@ -0,0 +1,4 @@
+angular.module('ExamenPhp').controller('logoutController', ['$scope', '$sessionStorage', '$location', function ($scope, $sessionStorage, $location) {
+    delete $sessionStorage.usuario;
+    $location.path('/');
+  }]);
